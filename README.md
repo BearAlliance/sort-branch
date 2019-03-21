b615055dd80ffb6b5ef28726025117eed0b4ec03
Nick Cacace <nick.cacace@adp.com>
2019-03-21 13:46:10 -0400

    feat: basic setup

---
+++ README.md
@@ -0,0 +1,20 @@
+# sort-branch
+
+A small script that prints your git branches in a useful way
+
+[stack overflow](https://stackoverflow.com/questions/5188320/how-can-i-get-a-list-of-git-branches-ordered-by-most-recent-commit) has a variety of opinions on how you can achieve this. I like this implementation the best
+
+## Installation
+
+```bash
+npm i -g sort-branch
+```
+
+## Use
+
+```bash
+sort-branch
+```
+
+Output
+![Output](./img/output.png)
---
+++ index.js
@@ -0,0 +1,10 @@
+const exec = require("shell-exec");
+
+exec(
+  "git for-each-ref --sort=-committerdate refs/heads --format='%(HEAD)%(color:yellow)%(refname:short)|%(color:bold green)%(committerdate:relative)|%(color:blue)%(subject)|%(color:magenta)%(authorname)%(color:reset)' --color=always|column -ts'|'})"
+);
+
+// Alternative configuration
+// exec(
+//   "git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+// );
